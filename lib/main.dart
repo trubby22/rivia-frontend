687f24d3b5a20fa1e96a8c77d726ae092f5c8f6d
Yitang Chen <yc4120@ic.ac.uk>
2022-06-06 23:05:41 +0100

    [yc4120] (feat) Can click meetings in dashboard to go to the review page

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:rivia/routes/review.dart';
 import 'package:rivia/routes/create_meeting.dart';
 import 'package:rivia/routes/dashboard_assigned.dart';
 import 'package:rivia/routes/dashboard_unassigned.dart';
+import 'package:rivia/routes/login.dart';
 import 'package:rivia/routes/review.dart';
 import 'package:rivia/routes/welcome_screen.dart';
 import 'package:rivia/routes/meeting_summary.dart';
