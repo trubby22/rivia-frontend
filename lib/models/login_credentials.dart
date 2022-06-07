43c8e7c83c4d0062d08ff36e8a402282004f34d5
Piotr Błaszyk <piopio555888@gmail.com>
2022-06-07 13:58:01 +0100

    [psb120] (feature) Added sign-up page for registering a new account

--- lib/models/login_credentials.dart
+++ lib/models/login_credentials.dart
@@ -6,19 +6,27 @@ String salt = 'yNwYRfX9mxQ-An#8sUw_a7LQgPaRe+a4';
 class LoginCredentials {
   final String loginHash;
   final String passwordHash;
+  final String? firstName;
+  final String? surname;
 
   LoginCredentials({
     required String login,
     required String password,
+    this.firstName,
+    this.surname,
   })  : loginHash = Crypt.sha256(login, salt: salt).toString(),
         passwordHash = Crypt.sha256(password, salt: salt).toString();
 
   LoginCredentials.fromJson(Map<String, dynamic> json)
       : loginHash = json[Fields.email],
-        passwordHash = json[Fields.password];
+        passwordHash = json[Fields.password],
+        firstName = json[Fields.name],
+        surname = json[Fields.surname];
 
   Map<String, dynamic> toJson() => {
         Fields.email: loginHash,
         Fields.password: passwordHash,
+        Fields.name: firstName,
+        Fields.surname: surname,
       };
 }
