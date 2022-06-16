9e059ed0fff8e583bfb8b2a179efc9bc1156282d
Yitang Chen <yc4120@ic.ac.uk>
2022-06-16 11:21:06 +0100

    Nullable datetime fromJSON

--- lib/models/date_time_json.dart
+++ lib/models/date_time_json.dart
@@ -1,6 +1,15 @@
 extension DateTimeJson on DateTime {
-  static DateTime? fromJSON(double json) =>
-      DateTime.fromMillisecondsSinceEpoch((json * 1000).toInt());
+  static DateTime? fromJSON(dynamic json) {
+    if (json.runtimeType == double) {
+      return DateTime.fromMillisecondsSinceEpoch((json * 1000).toInt());
+    }
+
+    if (json.runtimeType == int) {
+      return DateTime.fromMillisecondsSinceEpoch((json * 1000));
+    }
+
+    return null;
+  }
 
   double toJSON() => millisecondsSinceEpoch / 1000;
 }
